862c81905df021170cddd6188533b24a5511fc42
German Kröger <116314842+zaknein@users.noreply.github.com>
2025-09-24 10:35:26 -0300

    import  the json and http client

--- src/main/java/com/zaknein/TMDB_CLI_Tool/App.java
+++ src/main/java/com/zaknein/TMDB_CLI_Tool/App.java
@@ -1,5 +1,12 @@
 package com.zaknein.TMDB_CLI_Tool;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class App {
 
     public static void main(String[] args) {
