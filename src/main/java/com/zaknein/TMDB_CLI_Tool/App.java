f2c1dafcbec0bf5d72ca9c0beac0e741c213f5bc
German Kröger <116314842+zaknein@users.noreply.github.com>
2025-10-13 18:48:45 -0300

    logic cape
    separete the logic cape and coment the obsolets imports and code, will delete once check by senior

--- src/main/java/com/zaknein/TMDB_CLI_Tool/App.java
+++ src/main/java/com/zaknein/TMDB_CLI_Tool/App.java
@@ -1,27 +1,28 @@
 package com.zaknein.TMDB_CLI_Tool;
 
-import java.net.URI;
+// import java.net.URI;
 import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+// import java.net.http.HttpRequest;
+// import java.net.http.HttpResponse;
 import com.beust.jcommander.JCommander;
 
 import com.zaknein.TMDB_CLI_Tool.comandos.TypeCommand;
-import com.zaknein.TMDB_CLI_Tool.dominio.Movies;
-import com.zaknein.TMDB_CLI_Tool.dominio.MovieResponse;
+// import com.zaknein.TMDB_CLI_Tool.dominio.Movies;
+// import com.zaknein.TMDB_CLI_Tool.dominio.MovieResponse;
+import com.zaknein.TMDB_CLI_Tool.logica.SendToApi;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+// import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class App {
     private final HttpClient httpClient = HttpClient.newBuilder().version(HttpClient.Version.HTTP_2).build();
 
-    //Instanciar objectmapper
-    private static ObjectMapper objectMapper = new ObjectMapper();
+    // //Instanciar objectmapper
+    // private static ObjectMapper objectMapper = new ObjectMapper();
     public static void main(String[] args) {
 
         // Instanciamos 
         TypeCommand typecommand = new TypeCommand();
-        
+        SendToApi sendToApi = new SendToApi();
 
 
         JCommander jc = JCommander.newBuilder()
@@ -49,7 +50,7 @@ public class App {
                     return;
             }
             System.out.println(type);
-            sendGet(type);
+            sendToApi.sendGet(type);
 
         } catch (Exception e) {
             System.err.println("Error para procesar el tipo");
@@ -59,40 +60,40 @@ public class App {
 
     }
 
-    private static void sendGet(String type) throws Exception {
-        String token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNzFlM2JhZjQ4MDA2MWFkOTEyNzNkMmFkNjg2NTQxYiIsIm5iZiI6MTc1OTE4MTA4NC41MDcsInN1YiI6IjY4ZGFmOTFjODExN2FlNWUwN2JiMTE4MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AQRdGET_EJ-29hM9za3ti1GJut3sml4NqBUQqLnEXz8";
-        try {
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create("https://api.themoviedb.org/3/movie/" + type + "?language=en-US&page=1"))
-                    .header("accept", "application/json")
-                    .header("Authorization", "Bearer " + token)
-                    .method("GET", HttpRequest.BodyPublishers.noBody())
-                    .build();
-            HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println(response.statusCode());
+//     private static void sendGet(String type) throws Exception {
+//         String token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNzFlM2JhZjQ4MDA2MWFkOTEyNzNkMmFkNjg2NTQxYiIsIm5iZiI6MTc1OTE4MTA4NC41MDcsInN1YiI6IjY4ZGFmOTFjODExN2FlNWUwN2JiMTE4MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AQRdGET_EJ-29hM9za3ti1GJut3sml4NqBUQqLnEXz8";
+//         try {
+//             HttpRequest request = HttpRequest.newBuilder()
+//                     .uri(URI.create("https://api.themoviedb.org/3/movie/" + type + "?language=en-US&page=1"))
+//                     .header("accept", "application/json")
+//                     .header("Authorization", "Bearer " + token)
+//                     .method("GET", HttpRequest.BodyPublishers.noBody())
+//                     .build();
+//             HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+//             System.out.println(response.statusCode());
 
 
-            if(response.statusCode() == 200){
-                String responseBody = response.body();
+//             if(response.statusCode() == 200){
+//                 String responseBody = response.body();
 
 
-                MovieResponse movieResponse  = objectMapper.readValue(responseBody, MovieResponse.class);
+//                 MovieResponse movieResponse  = objectMapper.readValue(responseBody, MovieResponse.class);
 
-                for (int i = 0; i < Math.min(10, movieResponse.getResults().size()); i++) {
-                Movies movie = movieResponse.getResults().get(i);
+//                 for (int i = 0; i < Math.min(10, movieResponse.getResults().size()); i++) {
+//                 Movies movie = movieResponse.getResults().get(i);
 
-                System.out.println("---------------------------------------");
-                System.out.println("Movie title: " + movie.getTitle());
-                System.out.println("Release date: " + movie.getDate());
-                System.out.println("Overview: " + movie.getOverview());
-                }
+//                 System.out.println("---------------------------------------");
+//                 System.out.println("Movie title: " + movie.getTitle());
+//                 System.out.println("Release date: " + movie.getDate());
+//                 System.out.println("Overview: " + movie.getOverview());
+//                 }
 
-            }else{
-                System.out.println("Request failed. Status Code: " + response.statusCode());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//             }else{
+//                 System.out.println("Request failed. Status Code: " + response.statusCode());
+//             }
+//         } catch (Exception e) {
+//             e.printStackTrace();
+//         }
 
-    }
+//     }
 }
---
+++ src/main/java/com/zaknein/TMDB_CLI_Tool/logica/SendToApi.java
@@ -0,0 +1,56 @@
+package com.zaknein.TMDB_CLI_Tool.logica;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+
+import com.zaknein.TMDB_CLI_Tool.dominio.Movies;
+import com.zaknein.TMDB_CLI_Tool.dominio.MovieResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class SendToApi {
+    
+    public static void sendGet(String type) throws Exception {
+            
+        //Instanciar objectmapper
+        final ObjectMapper objectMapper = new ObjectMapper();  
+        
+        String token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNzFlM2JhZjQ4MDA2MWFkOTEyNzNkMmFkNjg2NTQxYiIsIm5iZiI6MTc1OTE4MTA4NC41MDcsInN1YiI6IjY4ZGFmOTFjODExN2FlNWUwN2JiMTE4MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AQRdGET_EJ-29hM9za3ti1GJut3sml4NqBUQqLnEXz8";
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://api.themoviedb.org/3/movie/" + type + "?language=en-US&page=1"))
+                    .header("accept", "application/json")
+                    .header("Authorization", "Bearer " + token)
+                    .method("GET", HttpRequest.BodyPublishers.noBody())
+                    .build();
+            HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.statusCode());
+
+
+            if(response.statusCode() == 200){
+                String responseBody = response.body();
+
+
+                MovieResponse movieResponse  = objectMapper.readValue(responseBody, MovieResponse.class);
+
+                for (int i = 0; i < Math.min(10, movieResponse.getResults().size()); i++) {
+                Movies movie = movieResponse.getResults().get(i);
+
+                System.out.println("---------------------------------------");
+                System.out.println("Movie title: " + movie.getTitle());
+                System.out.println("Release date: " + movie.getDate());
+                System.out.println("Overview: " + movie.getOverview());
+                }
+
+            }else{
+                System.out.println("Request failed. Status Code: " + response.statusCode());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
